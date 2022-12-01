9c4ab44d264eb884af221727af5264ae24641249
FaiSaLKhoKhR <faisal.khokhar177@gmail.com>
2022-12-01 11:53:59 +0500

    redux fornend design

---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+<nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">State Bank of Punjab</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div className="collapse navbar-collapse" id="navbarScroll">
+      <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style={{bsScrollHeight: "100"}}>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">About</a>
+        </li>
+      </ul>
+      <div>
+        <div disabled={true} className="btn btn-danger">Your Balance: 10,000</div>
+      </div>
+    </div>
+  </div>
+</nav>
+
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/components/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+        <h2>Buy Addidas Shoes in just (50 Rs)</h2>
+        <div className="btn btn-primary mx-2">-</div>
+        Add this item to cart
+        <div className="btn btn-primary mx-2">+</div>
+    </div>
+  )
+}
+
+export default Shop
